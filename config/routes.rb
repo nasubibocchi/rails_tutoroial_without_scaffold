67385f7d1a579fa54c80a8b471fc7572747a906d
nasubibocchi <matsuhito125@gmail.com>
2022-03-13 21:06:27 +0900

    Config signup path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   # get 'static_pages/home'
   # get 'static_pages/help'
   # get 'static_pages/about'
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/help', to:'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
